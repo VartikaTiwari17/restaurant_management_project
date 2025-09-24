258e9045dec2c04f25f17815bf5db6acc8cce359
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-09-24 15:10:16 +0530

    50725

---
+++ home/view.py
@@ -0,0 +1,8 @@
+from rest_framework.generics import ListAPIView
+from .models import MenuCategory
+from .serializer import MenuCategorySerializer
+
+
+class MenuCategoryListAPIView(ListAPIView):
+    queryset = MenuCategory.objects.all()
+    serializer_class = MenuCategorySerializer
