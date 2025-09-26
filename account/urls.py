a0fd8134a3ba5f2ba7a5c595969c6b70df856b33
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-09-26 18:40:17 +0530

    51018

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import *
+from .views import MenuItemViewSet
+
+
+menu_item_update = MenuItemViewSet.as_view({"put": "update"})
+
 
 urlpatterns = [
-    
+   path("item/<int:pk>/update/", menu_item_update, name="menu-item-update"),   
 ]
