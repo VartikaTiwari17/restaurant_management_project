be57bcb8e56b5278d481a57876a4a2c28feb5166
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-09-26 18:25:34 +0530

    51018

--- account/models.py
+++ account/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+
+
+     def __str__(self):
+         return self.category_name
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=200)
+    description = models. TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    available = models.ForeignKey(Category, on_delete=models.CASCADE,related_name="menu_items")
+
+
+    def __str__(self):
+          return self.name
