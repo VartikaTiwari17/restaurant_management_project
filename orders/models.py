cac8b4994cee960d39284cff9b9fa8d2d8a8dfca
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-09-26 13:53:28 +0530

    51021

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Order(models.Model):
+     user = models.ForeignKey(User,on_delete=models.CASCADE, related_name="orders")
+     date = models.DateTimeField(auto_now_add=True)
+     total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+     def __str__(self):
+         return f"Order  {self.id} - {self.user.username}"
+
+
+class OrderItem(models.Model):
+    order = models.ForignKey(Order, on_delete=models.CASCADE, related_name="item")
+    product_name = models.CharField(max_length=255)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+     def __str__(self):
+        return f"{self.product_name}  (x{self.quantity})"
