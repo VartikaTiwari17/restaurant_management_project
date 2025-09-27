58395425689a7870bde10358191a26dbd45f33c6
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-09-27 19:39:45 +0530

    51698

--- account/models.py
+++ account/models.py
@@ -2,19 +2,12 @@ from django.db import models
 
 
 
-class Category(models.Model):
-    category_name = models.CharField(max_length=100)
 
-
-     def __str__(self):
-         return self.category_name
-
-
-class MenuItem(models.Model):
-    name = models.CharField(max_length=200)
-    description = models. TextField(blank=True)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    available = models.ForeignKey(Category, on_delete=models.CASCADE,related_name="menu_items")
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone_numbar = models. CharField(max_length=20)
+    operating_days = models.CharField(max_length=100, help_text="Enter days like: Mon, Tue, Wed, Thu, Fri,Sat,Sun ")
 
 
     def __str__(self):
