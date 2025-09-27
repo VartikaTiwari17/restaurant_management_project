10041f9daa1de57a331c2faaf85e4c4e7310ade5
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-09-27 20:12:58 +0530

    51695

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics, permissions
+from .models import Order
+from .serializers import OrderSerializer
+
+
+class OrderDetailView(denerics.RetrieveAPIView):
+     queryset = Order.objects.all()
+     serializer_class = OrderSerializer
+     permission_classes = [permissions.IsAuthenticated]
+     lookup_field = 'id'
