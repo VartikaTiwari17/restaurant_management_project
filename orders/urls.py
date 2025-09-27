3be6b92bb8ae62db90f0cbcd420feb4f38694f31
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-09-27 20:09:34 +0530

    51695

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import *
+from .views import  OrderDetailView
+
 
 urlpatterns = [
-    
+    path ('<int:id>/', OrderDetailView.as_view(), name='order-detail'),
 ]
