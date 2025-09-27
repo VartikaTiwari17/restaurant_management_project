fbef862d4d45a4ca1d858b81c6ffce2750f1f2ad
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-09-27 19:45:43 +0530

    51697

--- account/models.py
+++ account/models.py
@@ -7,8 +7,10 @@ class Restaurant(models.Model):
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     phone_numbar = models. CharField(max_length=20)
-    operating_days = models.CharField(max_length=100, help_text="Enter days like: Mon, Tue, Wed, Thu, Fri,Sat,Sun ")
-
+    operating_days = models.CharField(
+        max_length=100, 
+        help_text="Enter days like: Mon, Tue, Wed, Thu, Fri,Sat,Sun "
+        )
 
     def __str__(self):
           return self.name
