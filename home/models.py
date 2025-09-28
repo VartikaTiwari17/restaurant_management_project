00e9953cf36d58af6dc5cdbb62810f3d3a33f9c9
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-09-28 07:55:24 +0530

    52013

--- home/models.py
+++ home/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 
-class MenuCategory(models.Model):
-     name = models.CharField(max_length=100, unique=True)
+
+class ContactFromSubmission(models.Model):
+     name = models.CharField(max_length=255)
+     email = models.EmailField()
+     message = models.TextField()
+     submitted_at = models.DataTimeField(auto_now_add=True)
+
 
 
      def __str__(self):
-        return self.name
+        return f"{self.name} - {self.email}"
