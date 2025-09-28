a36250829ca443ddaa1458727ea04353d63298a1
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-09-28 08:24:10 +0530

    52015

--- orders/models.py
+++ orders/models.py
@@ -1,23 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class Order(models.Model):
-     user = models.ForeignKey(User,on_delete=models.CASCADE, related_name="orders")
-     date = models.DateTimeField(auto_now_add=True)
-     total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('PROCESSING', 'Processing'),
+        ('CANCELLED', 'Cancelled'),
+        ('COMPLETED', 'Completed'),
+    ]
+
+
+    user = models.ForignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='orders')
+    order_id = models.CharField(max_length=20, unique=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
+    created_at  = models.DateTimeField(auto_now_add=True)
 
 
      def __str__(self):
-         return f"Order  {self.id} - {self.user.username}"
-
-
-class OrderItem(models.Model):
-    order = models.ForignKey(Order, on_delete=models.CASCADE, related_name="item")
-    product_name = models.CharField(max_length=255)
-    quantity = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-
-     def __str__(self):
-        return f"{self.product_name}  (x{self.quantity})"
+        return f"Order {self.order_id}- {self.status}"
