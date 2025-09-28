e261d92eb1febdcbbc4edb7464f90cc82c64dae8
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-09-28 08:55:50 +0530

    52016

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from decimal import decimal
+from home.models import MenuItem
 
 
 class Order(models.Model):
@@ -19,3 +21,23 @@ class Order(models.Model):
 
      def __str__(self):
         return f"Order {self.order_id}- {self.status}"
+
+
+    def calculate_total(self):
+        total = Decimal('0.00')
+        for item in self.items.all():
+             total  += item.price * item.quantity
+           return total
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='item')
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.PostiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('0.00'))
+
+
+
+    def __str__(self):
+        return f"{self.quantity} x {self.menu_item.name} for Order {self.order.order_id}"
+        
