4e41823536a0a4c04d901a90f9c2e32063146bca
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-09-28 08:03:27 +0530

    52013

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .view import MenuCategoryListAPIView
+from .view import ContactFormSubmissionView
 
 
 urlpatterns = [
-    path('api/menu-categories/', MenuCategoryListAPIView.as_view() name='menu-categories-list'),
+    path('api/contact/', ContactFormSubmissionView.as_view() name='contact-form-submit'),
 ]
