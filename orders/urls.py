49272d4cdf970d6922d359f8f85d71019a4d7c17
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-09-28 08:31:14 +0530

    52015

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
-from .views import  OrderDetailView
+from django.urls import path, include
+from rest_framework.routers import DeafultRouter
+from .view import OrderViewSet
 
+
+router = DefaultRouter()
+router.register(r'orders', OrderViewSet, basename='order')
 
 urlpatterns = [
-    path ('<int:id>/', OrderDetailView.as_view(), name='order-detail'),
+    path ('api/', include(router.urls)),
 ]
