97792ed1b864dcd1455f34659e6d3611e965e79c
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-05 19:05:59 +0530

    53655

--- home/models.py
+++ home/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 
-class MenuItem(models.Model):
-     name = models.CharField(max_length=100)
-      price = models.DecimalField(max_digits=7, decimal_places=2)
-      is_daily_special = models.BooleanField(default=False)
-
-
-
-     def __str__(self):
+class MenuCategory(models.Model):
+     name = models.CharField(max_length=100, unique=True)
+      
+    def __str__(self):
         return self.name
