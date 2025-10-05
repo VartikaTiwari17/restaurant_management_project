98ea411640eb9621bab77dd05eec122f70192611
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-05 19:26:07 +0530

    53653

--- home/serializer.py
+++ home/serializer.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
-from .models import MenuCategory 
+from .models import UserReview
 
 
-class MenuCategorySerializer(serializers.ModelSerializer):
+class UserReviewSerializer(serializers.ModelSerializer):
+    user_name = serializers.CharField(source='user.username', read_only=True)
       class Meta:
-         model = MenuCategory
-         fields = ['id', 'name']
+         model = UserReview
+         fields = ['id', 'name', 'menu_item', 'rating', 'comment', 'review_date']
+         read_only_fields = ['id', 'review_date', 'user_name']
+
+
+
+         def validate_rating(self,value):
+            if value <1 or value>5:
+                raise serializers.ValidationError("rating must be between 1 and 5.")
+            return value
          
