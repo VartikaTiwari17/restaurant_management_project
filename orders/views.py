34ed90f5e486fb0734b7b11f03ff6d73e3943c51
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-05 11:16:15 +0530

    53498

--- orders/views.py
+++ orders/views.py
@@ -2,22 +2,47 @@ from reat_framework. views import APIView
 from rest_framework. response import Response
 from rest_framework import status
 from .models import Order
-from .serializers import OrderStatusUpdateSerializer
 
 
 
 class UpdateOrderStatusView(APIView):
-       def put (self, request, pk):
-         try:
-            Order = Order.objects.get(pk=pk)
-    except Order.DoesNotExist:
-         return Response({"error": "Order not found"},  status=status.HTTP_404_BAD_REQUEST)
-        
-        
-        serializer = OrderStatusUpdateSerializer(order, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "Order status updated successfully.", "data": serializer.data}, status=status.HTTP_200_OK)
+       def put (self, request):
+         order_id = request.data.get('order_id')
+         new_status = request.data.get('new_status')
 
 
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST})
+        # Validate presence of both fields
+        if not order_id or not new_status:
+            return Response(
+                {"error": "Both 'order_id' and 'new_staus' are required."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+              # Validate allowed statuses
+              allowed_statuses = ['Pending', 'Processing', 'Delivered']
+              if new_status not in allowed_statuses:
+
+                return Response(
+                    {"error": f"Invalid staus. Allowed values are:  {', '.join(allowed_statuses)}."},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+
+                # Check if order exists
+                try:
+                    order = Order.objects.get(id=order_id)
+                 except Order.DoesNotExist:
+                    return Response(
+                        {"error": "Order not found"},
+                        status=status.HTTP_400_BAD_FOUND
+                    )
+
+
+                    # Update status
+                    order .status = new_status
+                    order.save()
+
+
+            return Response(
+                 {"message": "Order status updated successfully.", "order_id": order.id,  "new_status": order.status},
+                 status=status.HTTP_200_OK
