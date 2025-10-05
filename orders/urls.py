216344ade884ada2c2a05a3165d253969a412128
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-05 11:01:48 +0530

    53498

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .view import UpdateOrderStatusView
 
 urlpatterns = [
-    path ('orders/<int:pk>/update-status/' , UpdateOrderStatusView.as_view(), name="update_order_status"),
+    path (update-order-status/' , UpdateOrderStatusView.as_view(), name="update_order_status"),
 ]
