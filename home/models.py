b174a8e3057b76dd190a5e1fb31149d7f30a9720
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-06 07:10:57 +0530

    53676

--- home/models.py
+++ home/models.py
@@ -3,9 +3,13 @@ from django.db import models
 
 class MenuItem(models.Model):
      name = models.CharField(max_length=100)
-     price =  models.DecimalField(max_digits=8, decimal_places=2)
-     is_available = models.BooleanField(default=True)
-
+     address = models.TextField()
+     phone_number = models.CharField(max_length=15)
+     opening_hours = models.CharField(max_length=100)
+     email = models.EmailField(blank=True, null=True)
+     website = models.URLField(blank=True, nill=True)
+     description = models.TextField(blank=True, null=True)
+     
      
       def __str__(self):
         return self.name
