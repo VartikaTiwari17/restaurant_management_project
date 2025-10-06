b1a3679d071c30a5936f6b774e91d1400e7372fd
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-06 06:49:41 +0530

    53679

--- home/models.py
+++ home/models.py
@@ -3,20 +3,9 @@ from django.contrib.auth.models import User
 from .models import MenuCategory
 
 class MenuItem(models.Model):
-     name = models.CharField(max_length=100)
-     category = models .ForeignKey(MenuCategpory, on_delete=models.CASCADE)
-     price = models.DecimalField(max_digits=6, decimal_places=2)
+     name = models.CharField(max_length=100, unique=True)
      description = models.TextField(blank=True, null=True)
       def __str__(self):
         return self.name
 
 
-class UserReview(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
-    menu_item = model.ForeignKey('MenuItem', on_delete=models.CASCADE, related_name="reviews")
-    rating= = models.IntegerField()
-    comment = models.TextField()
-    review_date = models.DataTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Review by {self.user.username} for {self.menu_item.name}"
