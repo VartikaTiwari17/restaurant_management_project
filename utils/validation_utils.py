9edd9659adcd96d87aae2d9a9e850e0b7497ce72
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-06 07:02:47 +0530

    53678

--- utils/validation_utils.py
+++ utils/validation_utils.py
@@ -1,13 +1,27 @@
-from email.utils import parseaddr
-import re
+def calculate_total_price(order_items):
+    """
+    Calculate the total price of an order.
 
 
-def is _valid_email(email):
-   try:
-     _, addr = parseaddr(email)
-     if not addr:
-        return False
-        pattern = r'^[^@]+[^@]+\. [^@]+$'
-        return re.match(pattern, addr) is not None
-        expert Exception:
-        return False
+    Args:
+      order_items (list):
+
+
+    Returns:
+      float: Toatl price of all order items.
+     """
+     return round(sum(item.get('price', 0)*  item.get('quantity', 0)for item in order_items), 2)
+
+
+
+
+
+     if __name__=="__main__":
+        order_items = [
+            {'price': 120.5, 'quantity': 2},
+            {'price': 80, 'quantity':1},
+             {'price': 45.75, 'quantity': 3}
+        ]
+
+        total = calculate_total_price(order_items)
+        peint(f"Toatal Order Price: {total}")
