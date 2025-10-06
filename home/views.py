e783faa02ad4204c4a2e80f33deb5520db7567b4
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-06 06:53:03 +0530

    53679

--- home/views.py
+++ home/views.py
@@ -4,20 +4,7 @@ from .models import UserReview
 from .serializers import UserReviewSerializer
 
 
-class UserReviewCreateAPIView(generics.CreateAPIView):
-    queryset = UserReview.objects.all()
-    serializer_class = UserReviewSerializer
-    permissions_classes = [IsAuthenticatedOrReadOnly]
+class MenuCategoryViewSet(viewset.ModelViewSet):
+    queryset = MenuCategory.objects.all()
+    serializer_class = MenuCategorySerializer
 
-
-    def perform_create(self,serializer):
-        serializer.save(user=self.request.user)
-     
-
-     class MenuItemReviewAPIView(generics.ListAPIView):
-        serializer_class = UserReviewSerializer
-
-
-        def get_queryset(self):
-            menu_item = self.kwargs.get('menu_item_id')
-            return UserReview.objects.filter(menu_item__id=menu_item_id)
