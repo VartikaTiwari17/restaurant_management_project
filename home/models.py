29327b636fb0d63a8fe1feddb206b807682e7ec6
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-06 07:05:37 +0530

    53677

--- home/models.py
+++ home/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
-from .models import MenuCategory
+
 
 class MenuItem(models.Model):
-     name = models.CharField(max_length=100, unique=True)
-     description = models.TextField(blank=True, null=True)
+     name = models.CharField(max_length=100)
+     price =  models.DecimalField(max_digits=8, decimal_places=2)
+     is_available = models.BooleanField(default=True)
+
+     
       def __str__(self):
         return self.name
 
