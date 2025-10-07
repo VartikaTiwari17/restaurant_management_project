1268ba3051cfed5a3f246b5e72b67221c2cfe358
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-07 12:35:06 +0530

    53748

--- home/models.py
+++ home/models.py
@@ -2,11 +2,27 @@ from django.db import models
 
 
 class MenuItem(models.Model):
-     name = models.CharField(max_length=100, unique=True)
+     name = models.CharField(max_length=100)
+     description = models.TextField(blank=True, null=True)
+     price = models.FloatField()
+     discount_percent = models.FloatField(default=0.0, help_text="Discount percentage on the item (0-100")
+
     
-     
-     
-      def __str__(self):
+    def __str__(self):
         return self.name
+
+
+    def get_final_price(self) -> float:
+        """
+        Calculate the final price after applying discount.
+        Return a float rounded to 2 decimal places.
+        """
+        if self.discount_percent > 0:
+            discount_amount = (self.price * self.discount_percent) / 100
+            final_price = self.price - discount_amount
+        else:
+            final_price = self.price
+
+            return round(final_price,2)
 
 
