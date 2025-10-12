92b7b7a1164a0390fc80d68e76cb0c7ec70cff0e
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-12 19:01:08 +0530

    54807

--- order/models.py
+++ order/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class OrderStatus(models.Model):
        name = models.CharField(max_length=50, unique=True)
+       user = models. DateTimeField(auto_now_add=True)
 
 
        def __str__(self):
