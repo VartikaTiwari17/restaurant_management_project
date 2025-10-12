aa8714352c9b3d8e7c435a7c6a7b2639f7407f1d
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-12 18:27:25 +0530

    54794

--- home/models.py
+++ home/models.py
@@ -1,25 +1,15 @@
 from django.db import models
 
 
-class MenuItem(models.Model):
-     name = models.CharField(max_length=100)
-     location = models.CharField(max_length=200, blank=True, null=True)
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    contact_number = models.CharField(max_length=15)
+    opening_hours = models.CharField(
+        max_length=50,
+        help_text="Format example: 11:00 AM - 11:00 PM (EST)"
+    )
 
-    
-    def __str__(self):
+
+     def __str__(self):
         return self.name
-
-
-    class OpeningHour(models.Model):
-        Days_OF_WEEK = [Monday Tuesday wednesday thursday friday saturday sunday
-            
-        ]
-        if self.discount_percent > 0:
-            discount_amount = (self.price * self.discount_percent) / 100
-            final_price = self.price - discount_amount
-        else:
-            final_price = self.price
-
-            return round(final_price,2)
-
-
