1136843f02eb73e89c7209bece18d1b3d4ddc887
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-12 17:43:33 +0530

    54037

--- home/models.py
+++ home/models.py
@@ -3,20 +3,17 @@ from django.db import models
 
 class MenuItem(models.Model):
      name = models.CharField(max_length=100)
-     description = models.TextField(blank=True, null=True)
-     price = models.FloatField()
-     created_at = models.FloatField(default=0.0, help_text="Discount percentage on the item (0-100")
+     location = models.CharField(max_length=200, blank=True, null=True)
 
     
     def __str__(self):
         return self.name
 
 
-    def get_final_price(self) -> float:
-        """
-        Calculate the final price after applying discount.
-        Return a float rounded to 2 decimal places.
-        """
+    class OpeningHour(models.Model):
+        Days_OF_WEEK = [Monday Tuesday wednesday thursday friday saturday sunday
+            
+        ]
         if self.discount_percent > 0:
             discount_amount = (self.price * self.discount_percent) / 100
             final_price = self.price - discount_amount
