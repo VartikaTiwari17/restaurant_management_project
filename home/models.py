b179fbe241a8cde44f9f10397e3db8f9d6278a1d
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-12 18:37:46 +0530

    54806

--- home/models.py
+++ home/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 
+
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+
+    def __str__(self)
+    return self.name
+
+
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     address = models.TextField()
