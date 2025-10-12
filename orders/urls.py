5d292878c9f2aa6f5fbcbbfce8fb570a07ffd617
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-12 18:49:31 +0530

    54811

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .view import get_order_status
 
 urlpatterns = [
-    path ('order-status/<int:order_id>/' , get_order_status, name="get-order-status"),
+    path ('api/<order>/history/' , get_order_status, name="get-order-status"),
 ]
