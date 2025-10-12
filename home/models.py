583287f140c53d6e8b478d3adae1de5ad63c593c
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-12 18:35:51 +0530

    54802

--- home/models.py
+++ home/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Restaurant(models.Model):
+class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     address = models.TextField()
     contact_number = models.CharField(max_length=15)
