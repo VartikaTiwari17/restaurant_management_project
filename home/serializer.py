34890e1166c90d8958618566884f037547f1103c
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-15 09:39:23 +0530

    55286

--- home/serializer.py
+++ home/serializer.py
@@ -1,27 +1,13 @@
 from .django.db import ModelSerializer
-
-
-class FAQ(models.Model):
-    question models.CharField(max_length=255)
-    answer = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-
-    def __str__(self):
+from .models import UserReview
 
 
 
 
-class MenuCategorySerializer(serializer.ModelSerializer):
-    class Meta:
-        model = MenuCategory
-        fields = ['id','name']
+class UserReviewSerializer(serializer.ModelSerializer):
+    user_name = serializer.CharField(source='user.username', read_only=True)
 
 
-
-        
-
-class MenuCategorySerializer(serializer.ModelSerializer):
     class Meta:
         model = MenuItem
         fields = ['id', name','description','price', 'image' 'is_available']   # Only essential details
