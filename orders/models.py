5b4608b5b18f4fcd3088679273107578c9d19bf8
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-15 09:16:09 +0530

    54816

--- orders/models.py
+++ orders/models.py
@@ -4,25 +4,26 @@ from djngo.contrib.auth.models import User
 
 
 class Order(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('PROCESSING', 'Processing'),
+        ('DELIVERIED', 'Delivered'),
+        ('CANCELLED', 'Cancelled'),
+    ]
+
+
+    
     user = models ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
 
 
     def __str__(self):
-        return f"Order  #{self.id} by {self.user.username}"
+        return f"Order  #{self.id} by {self.status}"
 
 
 
-
-
-   class OrderItem(models.Model):
-    order = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
-    menu_item = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
-    quantity = models.DateTimeField(auto_now_add=True)
-    
-    
-      def __str__(self):
-        return f"Order #{self.id} - {self.user.username}  ({self.status})"
 
 
     
