7e676c89562ed0cab7b3db5fc27439173d8b7a3d
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-15 09:24:26 +0530

    54814

--- home/models.py
+++ home/models.py
@@ -13,11 +13,9 @@ class MenuCategory(models.Model):
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
-    address = models.TextField()
-    contact_number = models.CharField(max_length=15)
-    opening_hours = models.CharField(
-        max_length=50,
-        help_text="Format example: 11:00 AM - 11:00 PM (EST)"
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
     )
 
 
