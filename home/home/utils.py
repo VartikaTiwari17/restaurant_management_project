3cddfe90feab69d5f833df20555b0369ebbffb72
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-18 16:27:15 +0530

    55747

--- home/home/utils.py
+++ home/home/utils.py
@@ -1,31 +1,11 @@
-import datetime import datetime, time
-
-def is_restaurant_open():
-    """
-    Checks if the restaurant is open basec on hardcoded opening hours.
-    Returns True if open, False if closed.
-    """
+from  .models import Table
 
 
 
-    #  Define opening hours (24-hour format)
-    # Monday=0, Tuesday=1, ..., Sunday=6
-    opening_hours = {
-        0: (time(9,0), time(22,0)),  # Monday
-        1: (time(9,0), time(22,0)), # Tuesday
-        2: (time(9,0), time(22,0)), # Wednesday
-        3: (time(9,0), time(22,0)), # Thursday
-        4: (time(9,0), time(22,0)), # Friday
-        5: (time(9,0), time(22,0)), # Saturday
-        6: None                     # Sunday closed 
-    }
- 
+def get_available_tables_by_capacity(num_guests):
+"""
 
+Return a QuerySet of tables that are available and can accommodate at least num_guests.
+"""
 
-    if open_time <= current_time <= close_time:
-        return True
-
-        
-    else:
-        return false
-        
+return Table.objects.filter(is_available=True, capacity__gte=num_guests)
