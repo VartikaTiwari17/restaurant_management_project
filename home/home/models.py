daeb84007a945a852b867a77ca14c2b9d0901bae
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-18 16:32:51 +0530

    55755

--- home/home/models.py
+++ home/home/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=Trye, null=True)
+    capacity = models.IntegerField(default=0, blank=True, null=True)
      
 
 
