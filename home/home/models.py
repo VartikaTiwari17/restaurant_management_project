1dc0bf4c1899b73d3e2393077762cf9ba586e2df
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-18 16:31:01 +0530

    55756

--- home/home/models.py
+++ home/home/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
 
-class Table(models.Model):
-     table_number = models.IntegerField(unique=True)
-     capacity = models.IntegerField()
-     is_available = models.BooleanField(default=True)
-     location = models.CharField(max_length=100)
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=Trye, null=True)
+     
 
 
 
      def __str__(self):
-        return f" Table {self.table_number} - {self.location}  ({'Available'  if self.is_available else 'Occupied})"
+        return self.name
