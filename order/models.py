5333ca9163f7471d8cad222fd23cb36f5d9084e2
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-18 16:50:14 +0530

    55752

--- order/models.py
+++ order/models.py
@@ -6,5 +6,8 @@ class OrderStatus(models.Model):
        user = models. DateTimeField(auto_now_add=True)
 
 
+
+customer_notes = models.TextField(blank=True, null=True)
+
        def __str__(self):
-          return self.name
+          return f"Order  #{self.id} - {self.customer.username}"
