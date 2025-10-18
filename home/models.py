954f4a112a7fe8fc5242aec2c44a6646b57c77cd
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-18 16:11:36 +0530

    55751

--- home/models.py
+++ home/models.py
@@ -15,11 +15,14 @@ class MenuItem(models.Model):
     Description  = models.TextField()
     price  = models. DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    category = models.ForeignKey('MenuCategory', on_delete=models.CASCADE)
+    allergens = models.CharField(max_length=255, blank=True, null=True)
     )
 
 
      def __str__(self):
+        if self.allergens:
+            return f"{self.name}  (Allergens:  {self.allergens})"
         return self.name
 
     
