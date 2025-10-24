2835c79843361c587460a6da8695ad9f464829b2
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-24 08:03:34 +0530

    56315

--- home/home/models.py
+++ home/home/models.py
@@ -26,7 +26,8 @@ class UserReviewa(models.Model):
     comment = models.TextField(blank=True, null=True)
     crated_at = models.DateTimeField(auto_now_add=True)
 
-
+  class Meta:
+    unique_together = ('menu_item', 'date')
 
     def __str__(self):
         return f'{self.user.username} - {self.menu_item.name}  ({self.rating}/5)'
