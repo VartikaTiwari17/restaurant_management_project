5a610c49f1d02f838a356affa517c8ab4bf82961
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-24 07:21:38 +0530

    55759

--- home/home/models.py
+++ home/home/models.py
@@ -1,13 +1,26 @@
-from django.db import models
+from django.db import models 
+from django.contrib.auth.models import User
 
 
-class Table(models.Model):
-    table_number = models.IntegerField(unique=True)
-    capacity = models.IntegerField()
-    is_available = models.BooleanField(default=True)
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
      
 
 
 
      def __str__(self):
-        return f"Table {self.table_number}   (Capacity:  {self.capacity})"
+        return self.name
+
+
+class UserReviewa(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.IntegerField()
+    comment = models.TextField(blank=True, null=True)
+    crated_at = models.DateTimeField(auto_now_add=True)
+
+
+
+    def __str__(self):
+        return f'{self.user.username} - {self.menu_item.name}  ({self.rating}/5)'
