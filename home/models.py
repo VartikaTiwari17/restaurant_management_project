a9b4b0be0185934a79d0b15c370c68bfe6fc9e83
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-24 07:28:50 +0530

    55757

--- home/models.py
+++ home/models.py
@@ -4,7 +4,29 @@ from django.db import models
 
 class Cuisine(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    
+
+
+          def __str__(self):
+           return self.name
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+
+    cuisine = models.ForeiKey(
+        Cuisine,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='menu_items'
+    )
+
+
+    def __str__(self):
         return self.name
 
     
