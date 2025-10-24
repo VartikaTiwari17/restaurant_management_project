180130a0b51e435ca3e2a4d7b26ab5a6281079a9
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-24 08:09:01 +0530

    56322

--- home/home/utils.py
+++ home/home/utils.py
@@ -10,5 +10,5 @@ def get_available_tables_by_capacity(num_guests):
 Return a QuerySet of tables that are available and can accommodate at least num_guests.
 """
 
-return [c for c cuisines if c]
+return f"${amount: .2f}"
 
