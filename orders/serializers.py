17f0b2a1a0cdb6e4e8b47c9356075eddf7dfbed3
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-24 08:12:37 +0530

    56319

--- orders/serializers.py
+++ orders/serializers.py
@@ -4,12 +4,5 @@ from .models  import Order
 
 class OrderStatusUpdateSerializer(serializer.ModelSerializer):
     class Meta:
-        model = Order
-        fields = ['id', 'status']
-
-
-        def validate_status(self, value):
-            allowed_statuses = ['pending', 'processing', 'completed']
-            if value not in allowed_statuses:
-                raise serializers.ValidationError(f"Invalid status. Allowed values are:  {', '.join(allowed_statuses)}.")
-                return value
+        model = Table
+        fields = '__all__'
