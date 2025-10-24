0e110a999012f946e9d070960910a66d451a397d
Vartika Tiwari <147368701+VartikaTiwari17@users.noreply.github.com>
2025-10-24 07:14:21 +0530

    55760

--- orders/orders/models.py
+++ orders/orders/models.py
@@ -1,23 +1,32 @@
 from django.db import models
 from decimal import decimal
-from .utils import calculate_discount   # assume yeh function utils.py me hai
-
-class Order(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    status = models.CharField(max_length=20, default='Pending')
 
 
-    def calculate_total(self):
-        total = Decimal('0.00')
-        for item in self.items.all():
-            amount = Decimal(item.price) * item.quantity
-            amount = calculate_discount(amount, item.discount)
-            total += amount
-        return round(total, 2)
+from .models import Order 
+from home.models import MenuItem 
+
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on+on_delete=models.CASCADE, related_name='items')
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.PositiveIntegerField(default=1)
-    discount = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name='items'
+    )
+
+
+    menu_item = models. ForeifnKey(
+        MenuItem,
+        on_delete=models.CASCADE,
+        related_name='order_items'
+    )
+    
+
+    quantity = models.IntegerField(default=1)
+    price_at_time_of_order = models. DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+
+    def __str__(self):
+        return f'{self.quantity} * {self.menu_item.name}'
