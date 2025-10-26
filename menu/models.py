2c470e64ebf3d7d2ab39f481a000f3e676303dd0
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-10-26 09:29:46 +0530

    Added MenuItemManager with get_budget_items method to MenuItem model

--- menu/models.py
+++ menu/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 
-class Category(models.Models):
-      category_name = models.CharField(max_length=100)
+# Custom manager for MenuItem
+class MenuItemManager(models.Manager):
+    def get_budget_items(self, max_price):
+        """
+        Returns MenuItem queryset filtered by price less than max_price
+        """
+        return self.filter(price__lte=max_price)
 
-
-      def __str__(self):
-          return self.category_name
-
+# Assuming MenuItem model exists
 class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_featured = models.BooleanField(default=False)
 
-    name = models.CharField(max_length=200)
-    location = models.CharField(max_length=200, blank=True, null=True)
+    objects = MenuItemManager()  # Attach custom manager
 
-     def __str__(self):
+    def __str__(self):
         return self.name
