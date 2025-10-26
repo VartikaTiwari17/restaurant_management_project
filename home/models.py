f1399cbb8294de8b590ef7b02f7ca807093b9dc3
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-10-26 09:34:55 +0530

    Added is_active BooleanField to MenuItem model with default=True

--- home/models.py
+++ home/models.py
@@ -1,37 +1,13 @@
 from django.db import models
-from django.utils import timezone
-
-
-
-class Ingredient(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-
-
-          def __str__(self):
-           return self.name
-
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_featured = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)  # New field added
 
-class DailySpecial(models.Model):
-    menu_item = models.ForeignKey(MenuItem, on_delte=models.CASCADE)
-    date = models.DateField()
-
-    
-
-    cuisine = models.ForeiKey(
-        Cuisine,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name='menu_items'
-    )
-
+    objects = models.Manager()  # Default manager (or custom manager if any)
 
     def __str__(self):
         return self.name
-
-    
