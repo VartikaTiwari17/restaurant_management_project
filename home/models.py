3d03b18ee43ab0fbc99302fb90dd51fac563cfec
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-10-26 09:36:51 +0530

    Added max_capacity IntegerField to Restaurant model (nullable)

--- home/models.py
+++ home/models.py
@@ -1,13 +1,8 @@
 from django.db import models
 
-class MenuItem(models.Model):
+class Restaurant(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    is_featured = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)  # New field added
-
-    objects = models.Manager()  # Default manager (or custom manager if any)
+    max_capacity = models.IntegerField(null=True, blank=True)  # New field added
 
     def __str__(self):
         return self.name
