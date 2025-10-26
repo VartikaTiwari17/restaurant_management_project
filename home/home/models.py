35781af979b6d8091df324376b25a08a2fb4a22d
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-10-26 09:06:19 +0530

    Added Ingredient model with name and unit_of_measure fields

--- home/home/models.py
+++ home/home/models.py
@@ -1,33 +1,8 @@
-from django.db import models 
-from django.contrib.auth.models import User
+from django.db import models
 
-
-class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-
-    is_featured = models.DecimalField(max_digits=6, decimal_places=2)
-
-
-    objects = MenuItemManager()
-     
-
-
-
-     def __str__(self):
-        retur self.name
-
-
-class UserReviewa(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE, related_name='reviews')
-    rating = models.IntegerField()
-    comment = models.TextField(blank=True, null=True)
-    crated_at = models.DateTimeField(auto_now_add=True)
-
-  class Meta:
-    unique_together = ('menu_item', 'date')
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    unit_of_measure = models.CharField(max_length=50)
 
     def __str__(self):
-        return f'{self.user.username} - {self.menu_item.name}  ({self.rating}/5)'
+        return self.name
