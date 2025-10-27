c719b66f55210faf1008268b43349be2d388915a
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-10-27 11:44:42 +0530

    Add signal to log creation of new reservations for auditing in Orders app

--- orders/apps.py
+++ orders/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
 
-
 class OrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'orders'
 
     def ready(self):
-        import orders.signals  #noqa
+        from django.db.models.signals import post_save
+        from .models import Reservation, log_new_reservation
+
+        post_save.connect(log_new_reservation, sender=Reservation)
