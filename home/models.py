1e6efbbc2fed18e0932406ba5ea1f4bbebd3d4c0
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-10-27 07:57:01 +0530

    Add get_available_items method to MenuItem model for filtering available items

--- home/models.py
+++ home/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
-class Restaurant(models.Model):
+class AvailableMenuItemManager(models.Manager):
+    def get_available_items(self):
+        return self.filter(is_available=True)
+
+class MenuItem(models.Model):
     name = models.CharField(max_length=100)
-    max_capacity = models.IntegerField(null=True, blank=True)  # New field added
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+
+    objects = models.Manager()  # Default manager
+    available = AvailableMenuItemManager()  # Custom manager
 
     def __str__(self):
         return self.name
