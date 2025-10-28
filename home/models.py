912dbfd58d59c18087cc890186047b71d95f656f
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-10-28 11:01:25 +0530

    Add has_delivery field to Restaurant model with default False

--- home/models.py
+++ home/models.py
@@ -1,11 +1,8 @@
-from django.db import models
-from django.utils import timezone
-
-class PromotionalOfferManager(models.Manager):
-    def upcoming_offers(self):
-        today = timezone.now().date()
-        return self.filter(
-            is_active=True,
-            start_date__lte=today,
-            end_date__gte=today
-        )
+from home.models import Restaurant
+r = Restaurant.objects.create(
+    name="Spice Villa",
+    address="Ayodhya",
+    contact_number="9876543210",
+    email="spicevilla@example.com"
+)
+print(r.has_delivery)
