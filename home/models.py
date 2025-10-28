a7f0c3ce42ce90e882c1aa102dbc6c93a017b23e
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-10-28 09:32:46 +0530

    Add custom ExpiredCouponManager with expired() method to filter out expired coupons

--- home/models.py
+++ home/models.py
@@ -1,44 +1,21 @@
 from django.db import models
-
-# Define dietary choices at the top of the file
-DIETARY_CHOICES = [
-    ('VEGAN', 'Vegan'),
-    ('VEGETARIAN', 'Vegetarian'),
-    ('GLUTEN_FREE', 'Gluten-Free'),
-    ('HALAL', 'Halal'),
-    ('NONE', 'None'),
-]
-
-class MenuCategory(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return self.name
+from django.utils import timezone
 
 
-class Allergen(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+class ExpiredCouponManager(models.Manager):
+    def expired(self):
+        """Return all coupons that are no longer valid."""
+        return self.filter(valid_until__lt=timezone.now())
+
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    valid_from = models.DateTimeField()
+    valid_until = models.DateTimeField()
+
+    # attach the custom manager
+    objects = ExpiredCouponManager()
 
     def __str__(self):
-        return self.name
-
-
-class MenuItem(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE, related_name='menu_items')
-    allergens = models.ManyToManyField(Allergen, blank=True)
-    
-    # 👇 New field added here
-    dietary_preference = models.CharField(
-        max_length=20,
-        choices=DIETARY_CHOICES,
-        default='NONE',
-        blank=True,
-        null=True,
-        help_text="Select the dietary preference of this menu item (if any)."
-    )
-
-    def __str__(self):
-        return self.name
+        return self.code
