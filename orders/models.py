bcf4567bd84b723141cf7a112ac76424f882f2ed
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-10-28 11:19:22 +0530

    Add LoyaltyProgram model for customer loyalty tiers

--- orders/models.py
+++ orders/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-class PaymentMethod(models.Model):
+class LoyaltyProgram(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    description = models.TextField(blank=True, null=True)
-    is_active = models.BooleanField(default=True)
+    points_required = models.IntegerField(unique=True)
+    discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField()
 
     def __str__(self):
-        return self.name
+        return f"{self.name} ({self.discount_percentage}% discount)"
