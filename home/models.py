f3fd1cbc398c741d8ffaad8a544a26a40ba3c476
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-10-28 11:06:53 +0530

    Add NutritionalInformation model linked to MenuItem for storing nutrition details

--- home/models.py
+++ home/models.py
@@ -1,8 +1,11 @@
-from home.models import Restaurant
-r = Restaurant.objects.create(
-    name="Spice Villa",
-    address="Ayodhya",
-    contact_number="9876543210",
-    email="spicevilla@example.com"
-)
-print(r.has_delivery)
+from django.db import models
+
+class NutritionalInformation(models.Model):
+    menu_item = models.ForeignKey('MenuItem', on_delete=models.CASCADE, related_name='nutrition_info')
+    calories = models.IntegerField()
+    protein_grams = models.DecimalField(max_digits=5, decimal_places=2)
+    fat_grams = models.DecimalField(max_digits=5, decimal_places=2)
+    carbohydrate_grams = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.menu_item.name} - {self.calories} kcal"
