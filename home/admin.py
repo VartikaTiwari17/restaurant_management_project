8ea854aa7284a0d11f55a40486f1fb8a15cc2f39
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-10-28 09:19:07 +0530

    Customize MenuItemAdmin to display visual availability indicator in Django Admin

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import RestaurantStaff, StaffShift
+from django.utils.html import format_html
+from .models import MenuItem
 
-admin.site.register(RestaurantStaff)
-admin.site.register(StaffShift)
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'is_active', 'availability_status')
+
+    def availability_status(self, obj):
+        """Display a green check if active, red cross if not."""
+        if obj.is_active:
+            return format_html('<span style="color: green;">✔️ Available</span>')
+        return format_html('<span style="color: red;">❌ Unavailable</span>')
+
+    availability_status.short_description = 'Availability'
