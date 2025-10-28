a2baea6fb7f7b79aa178cf72355960efab26de1b
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-10-28 09:10:10 +0530

    Add utility function is_restaurant_closed_on_holiday to check for holiday closures

--- home/home/utils.py
+++ home/home/utils.py
@@ -1,20 +1,12 @@
-# home/utils.py
-from datetime import datetime
-from django.utils import timezone
-from .models import DailyOperatingHours  # make sure this model exists
+from datetime import date
+from home.models import HolidayClosure
 
-def is_restaurant_open():
-    """Return True if the restaurant is currently open, else False."""
-    now = timezone.localtime()
-    current_day = now.strftime('%A')  # e.g., 'Monday'
-    current_time = now.time()
-
-    try:
-        today_hours = DailyOperatingHours.objects.get(day=current_day)
-    except DailyOperatingHours.DoesNotExist:
-        return False  # No data for today means closed
-
-    # Check if within today's operating hours
-    if today_hours.opening_time <= current_time <= today_hours.closing_time:
-        return True
-    return False
+def is_restaurant_closed_on_holiday(date_to_check: date) -> bool:
+    """
+    Returns True if the restaurant is closed on the given date
+    due to a full-day holiday closure, otherwise False.
+    """
+    return HolidayClosure.objects.filter(
+        date=date_to_check,
+        is_full_day_closure=True
+    ).exists()
