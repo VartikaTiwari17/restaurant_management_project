6e831fd149e4ea81ad1874ceba0b8fe84dfa8bd5
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-10-28 10:54:12 +0530

    Add custom model manager for PromotionalOffer to fetch ongoing/upcoming offers

--- home/models.py
+++ home/models.py
@@ -1,21 +1,11 @@
 from django.db import models
 from django.utils import timezone
 
-
-class ExpiredCouponManager(models.Manager):
-    def expired(self):
-        """Return all coupons that are no longer valid."""
-        return self.filter(valid_until__lt=timezone.now())
-
-
-class Coupon(models.Model):
-    code = models.CharField(max_length=50, unique=True)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    valid_from = models.DateTimeField()
-    valid_until = models.DateTimeField()
-
-    # attach the custom manager
-    objects = ExpiredCouponManager()
-
-    def __str__(self):
-        return self.code
+class PromotionalOfferManager(models.Manager):
+    def upcoming_offers(self):
+        today = timezone.now().date()
+        return self.filter(
+            is_active=True,
+            start_date__lte=today,
+            end_date__gte=today
+        )
