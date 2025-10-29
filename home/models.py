5797f64478d3fd53608bdd1f034a25503552a6d7
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-10-29 07:38:06 +0530

    Add is_gluten_free field to MenuItem model to mark gluten-free dishes

--- home/models.py
+++ home/models.py
@@ -1,10 +1,12 @@
-from django.db import models
-
-class DeliveryZone(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    min_order_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    delivery_fee = models.DecimalField(max_digits=10, decimal_places=2)
-    is_active = models.BooleanField(default=True)
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    calories = models.IntegerField(null=True, blank=True)
+    is_gluten_free = models.BooleanField(
+        default=False,
+        help_text='Indicates if the menu item is gluten-free.'
+    )
 
     def __str__(self):
         return self.name
