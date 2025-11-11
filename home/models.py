ad7482a8d30c7fb326285511e743a49bef6c7865
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-11-11 19:48:03 +0530

    Added VegetarianMenuItemManager for filtering vegetarian dishes in MenuItem model

--- home/models.py
+++ home/models.py
@@ -1,17 +1,25 @@
 from django.db import models
 
-class MenuItemManager(models.Manager):
+
+# Custom Manager for Vegetarian Menu Items
+class VegetarianMenuItemManager(models.Manager):
     def get_vegetarian_items(self):
+        """Return only vegetarian menu items."""
         return self.filter(is_vegetarian=True)
 
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     is_vegetarian = models.BooleanField(default=False)
+    stock_quantity = models.IntegerField(default=0, help_text="Current available stock of this item")
 
-    # Replace default manager
-    objects = MenuItemManager()
+    # Default Manager
+    objects = models.Manager()
+
+    # Custom Vegetarian Manager
+    vegetarian_objects = VegetarianMenuItemManager()
 
     def __str__(self):
         return self.name
