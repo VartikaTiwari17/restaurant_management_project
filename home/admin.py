d0664e53abb0f7775d2e24dbfad514504f077a55
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-11-12 10:09:58 +0530

    Added rating field to Feedback model for structured user ratings

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import RestaurantAnnouncement
+from .models import Feedback
 
-@admin.register(RestaurantAnnouncement)
-class RestaurantAnnouncementAdmin(admin.ModelAdmin):
-    list_display = ('title', 'publish_date', 'is_active')
-    list_filter = ('is_active', 'publish_date')
-    search_fields = ('title', 'content')
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'message', 'rating', 'status', 'created_at')
--- home/models.py
+++ home/models.py
@@ -1,10 +1,5 @@
-from django.db import models
-
-class RestaurantAnnouncement(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    publish_date = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.title
+rating = models.IntegerField(
+    null=True,
+    blank=True,
+    help_text="Rating from 1 to 5"
+)
