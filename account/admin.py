c6d2f0dbcfad72a9c97198205a930b70c1ba810e
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-11-12 10:27:02 +0530

    Registered LoyaltyPoint model in Django Admin with list display, search, and filter options

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import LoyaltyPoint
 
-admin.site.register(LoyaltyPoint)
+@admin.register(LoyaltyPoint)
+class LoyaltyPointAdmin(admin.ModelAdmin):
+    list_display = ('user', 'points', 'last_updated')
+    search_fields = ('user__username',)
+    list_filter = ('last_updated',)
