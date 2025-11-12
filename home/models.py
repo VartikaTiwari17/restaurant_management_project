d1d816da4fc360c89fa9bcbc1483282dd189f00f
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-11-12 10:23:24 +0530

    Added custom manager method get_by_price_range to MenuItem model for price-based filtering

--- home/models.py
+++ home/models.py
@@ -1,5 +1,20 @@
-rating = models.IntegerField(
-    null=True,
-    blank=True,
-    help_text="Rating from 1 to 5"
-)
+from django.db import models
+
+class MenuItemManager(models.Manager):
+    def get_by_price_range(self, min_price, max_price):
+        """Return menu items within the specified price range."""
+        return self.filter(price__gte=min_price, price__lte=max_price)
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    # ... other existing fields ...
+
+    # Attach the custom manager
+    objects = MenuItemManager()
+
+    def __str__(self):
+        return self.name
