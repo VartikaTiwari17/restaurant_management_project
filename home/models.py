c1ab918e8bb107131f03e35439cb028963d4da14
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-11-13 10:48:28 +0530

    Added custom VegetarianMenuItemManager with get_vegetarian_items() for MenuItem model

--- home/models.py
+++ home/models.py
@@ -1,11 +1,20 @@
-from django.db import models
+from django.db import models # type: ignore
 
-class Restaurant(models.Model):
+class VegetarianMenuItemManager(models.Manager):
+    def get_vegetarian_items(self):
+        return super().get_queryset().filter(is_vegetarian=True)
+
+
+class MenuItem(models.Model):
     name = models.CharField(max_length=255)
-    address = models.TextField()
-    contact_number = models.CharField(max_length=20)
-    # ✅ New field for image
-    featured_image = models.ImageField(upload_to='restaurant_images/', blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_vegetarian = models.BooleanField(default=False)
+    preparation_time_minutes = models.IntegerField(blank=True, null=True)
+
+    # ✅ Add the custom manager
+    objects = models.Manager()  # Default manager
+    vegetarian_objects = VegetarianMenuItemManager()  # Custom vegetarian manager
 
     def __str__(self):
         return self.name
