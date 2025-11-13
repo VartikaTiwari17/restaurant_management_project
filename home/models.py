7204fb6d3b350aa4b090a492de26ecf87aad3519
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-11-13 10:31:00 +0530

    Added VegetarianMenuItemManager to filter only vegetarian menu items in MenuItem model

--- home/models.py
+++ home/models.py
@@ -1,10 +1,17 @@
-from account import models
+from django.db import models
 
-
-class MenuCategory(models.Model):
+class VegetarianMenuItemManager(models.Manager):
+    def get_queryset(self):
+        # Return only vegetarian items
+        return super().get_queryset().filter(is_vegetarian=True)
+class MenuItem(models.Model):
     name = models.CharField(max_length=100)
-    # ✅ New field added
-    description = models.CharField(max_length=255, blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    is_vegetarian = models.BooleanField(default=False)
+    # ... other fields ...
+
+    # Custom manager for vegetarian items
+    vegetarian_objects = VegetarianMenuItemManager()
 
     def __str__(self):
         return self.name
