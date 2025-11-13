390165c0fb6db88e6bb2d99e54ecdcc51d0751f7
Vartika Tiwari <vartikatiwari688@gmail.com>
2025-11-13 10:46:35 +0530

    Added featured_image field to Restaurant model and thumbnail preview in Django Admin

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,14 @@
-from django.contrib import admin # pyright: ignore[reportMissingModuleSource]
-from .models import DailySpecial
-@admin.register(DailySpecial)
-class DailySpecialAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'is_active')
-    list_filter = ('is_active',)
+from django.contrib import admin
+from django.utils.html import format_html
+from .models import Restaurant
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'contact_number', 'display_featured_image_thumbnail')
+
+    def display_featured_image_thumbnail(self, obj):
+        if obj.featured_image:
+            return format_html('<img src="{}" width="60" height="60" style="object-fit: cover; border-radius: 5px;" />', obj.featured_image.url)
+        return "No Image"
+
+    display_featured_image_thumbnail.short_description = "Featured Image"
--- home/models.py
+++ home/models.py
@@ -1,11 +1,11 @@
-from account import models
+from django.db import models
 
-
-class DailySpecial(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    is_active = models.BooleanField(default=True)  # New field
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    contact_number = models.CharField(max_length=20)
+    # ✅ New field for image
+    featured_image = models.ImageField(upload_to='restaurant_images/', blank=True, null=True)
 
     def __str__(self):
         return self.name
